4f1dda309097bf4895891c6f96e4d7d12adfb282
walrus_operator <naignatiev@yandex.ru>
2023-04-28 16:35:12 +0300

    Run server

--- practice_strings_in_web/run.py
+++ practice_strings_in_web/run.py
@@ -1,15 +1,17 @@
 from flask import Flask
 
+print('Form url...\n')
 from task_url import url_link
+print('Create HTML...\n')
 from task_html import html_text
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='images')
 
 
 url_prefix = 'http://127.0.0.1:5000'
 if url_link.startswith(url_prefix):
-    route = url_link[len(url_prefix)]
+    route = url_link[len(url_prefix):]
 else:
     raise ValueError(f'Первая часть ссылки должна быть {url_prefix}')
 
@@ -17,3 +19,6 @@ else:
 @app.route(route)
 def index():
     return html_text
+
+
+app.run()
